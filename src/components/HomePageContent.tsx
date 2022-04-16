8e420551601876eea3d6e3ae55d03624d0de3f06
Jonathan Raphael <jonathan.raphael274@gmail.com>
2022-04-17 00:10:20 +0100

    adds extra images

--- src/components/HomePageContent.tsx
+++ src/components/HomePageContent.tsx
@@ -1,52 +1,43 @@
 import {FC} from 'react' 
-import sculptureA from '../images/laura-seaman-01RpyK6lKBs-unsplash.jpg'
-import sculptureB from '../images/sirirat-wuthipracharat-qytWcXSCDug-unsplash.jpg'
-import sculptureC from '../images/qingbao-meng-01_igFr7hd4-unsplash.jpg'
 import { NavBar } from './NavBar'
 
-export const HomePageContent:FC = () => {
-    return <div style={{color: "black", display: 'flex', flexDirection: 'column', justifyContent: 'center', gap:'20px', alignItems: 'center', fontSize: 'larger'}}>
+const ImageContainer:FC<{image: string}> = ({image}) => {
+    return  <div style={{display: 'grid', gridTemplateColumns: '1fr 1fr 1fr'}}>
+    <div style={{gridColumnStart: 2, gridColumnEnd: 3}}>
+        <img src={image} width={'100%'} alt="Nick's sculpture"/>
+    </div>
+    <div style={{gridColumnStart: 3, gridColumnEnd: 4, display: 'flex', flexDirection: 'column', justifyContent: 'flex-end',  marginLeft: '15px', fontSize: 'small'}}>
+        <p style={{margin: '0', fontStyle: 'italic', textAlign: 'left', fontSize: 'small', fontWeight: 'bold'}}>
+                    Untitled 2021
+        </p>
+        <p style={{margin: '0', fontStyle: 'italic', textAlign: 'left', fontSize: 'small'}}>
+                    Concrete sculpture and moving image, London 
+        </p>
+    </div>
+</div>
+}
+
+export const HomePageContent:FC<{images: Array<string>}> = ({images}) => {
+    return <div style={{color: "black", display: 'flex', flexDirection: 'column', justifyContent: 'center', gap:'20px', alignItems: 'center', fontSize: 'larger', marginBottom: '10px'}}>
         <div style={{display: 'grid', gridTemplateColumns: '1fr 1fr 1fr', marginTop: '100px'}}>
             <div style={{gridColumnStart: 1, gridColumnEnd: 2}}>
                 <NavBar/>
             </div>
             <div style={{gridColumnStart: 2, gridColumnEnd: 3}}>
-                <img src={sculptureA} width={'100%'} alt="Nick's sculpture"/>
+                <img src={images[0]} width={'100%'} alt="Nick's sculpture"/>
             </div>
             <div style={{gridColumnStart: 3, gridColumnEnd: 4, display: 'flex', flexDirection: 'column', justifyContent: 'flex-end',  marginLeft: '15px', fontSize: 'small'}}>
-                    <p style={{margin: '0', fontWeight: 'bold'}}>
-                        Heres is my artwork
-                    </p>
-                    <p style={{ margin: '0'}}>
-                        Nick Collee
-                    </p>
+                <p style={{margin: '0', fontStyle: 'italic', textAlign: 'left', fontSize: 'small', fontWeight: 'bold'}}>
+                        Untitled 2021
+                </p>
+                <p style={{margin: '0', fontStyle: 'italic', textAlign: 'left', fontSize: 'small'}}>
+                        Concrete sculpture and moving image, London 
+                </p>
             </div>
         </div>
-        <div style={{display: 'grid', gridTemplateColumns: '1fr 1fr 1fr'}}>
-            <div style={{gridColumnStart: 2, gridColumnEnd: 3}}>
-                <img src={sculptureB} width={'100%'} alt="Nick's sculpture"/>
-            </div>
-            <div style={{gridColumnStart: 3, gridColumnEnd: 4, display: 'flex', flexDirection: 'column', justifyContent: 'flex-end',  marginLeft: '15px', fontSize: 'small'}}>
-                    <p style={{margin: '0', fontWeight: 'bold'}}>
-                        Heres is my artwork
-                    </p>
-                    <p style={{ margin: '0'}}>
-                        Nick Collee
-                    </p>
-            </div>
-        </div>
-        <div style={{display: 'grid', gridTemplateColumns: '1fr 1fr 1fr'}}>
-            <div style={{gridColumnStart: 2, gridColumnEnd: 3}}>
-                <img src={sculptureC} width={'100%'} alt="Nick's sculpture"/>
-            </div>
-            <div style={{gridColumnStart: 3, gridColumnEnd: 4, display: 'flex', flexDirection: 'column', justifyContent: 'flex-end',  marginLeft: '15px', fontSize: 'small'}}>
-                    <p style={{margin: '0', fontWeight: 'bold'}}>
-                        Heres is my great artwork
-                    </p>
-                    <p style={{ margin: '0'}}>
-                        Nick Collee
-                    </p>
-            </div>
-        </div>
+        <ImageContainer image={images[1]}/>
+        <ImageContainer image={images[2]}/>
+        <ImageContainer image={images[0]}/>
+        <ImageContainer image={images[1]}/>
     </div>
 }
--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -2,12 +2,16 @@ import {FC, useState} from 'react'
 import { Entry } from './Entry'
 import { EmptyBar } from './EmptyBar'
 import { HomePageContent } from './HomePageContent'
+import sculptureA from '../images/laura-seaman-01RpyK6lKBs-unsplash.jpg'
+import sculptureB from '../images/sirirat-wuthipracharat-qytWcXSCDug-unsplash.jpg'
+import sculptureC from '../images/qingbao-meng-01_igFr7hd4-unsplash.jpg'
 
 export const Homepage:FC = () => {
     const [entered, setEntered] = useState(false)
+    const images = [sculptureA, sculptureB, sculptureC]
 
     return <div>
         <EmptyBar/>
-        {entered ? <HomePageContent/> : <Entry setEntered={setEntered}/>}
+        {entered ? <HomePageContent images={images}/> : <Entry setEntered={setEntered}/>}
     </div>
 }
