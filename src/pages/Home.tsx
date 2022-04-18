c5ecd2ec24ad011406e72c03ae27606936fc31c9
Jonathan Raphael <jonathan.raphael274@gmail.com>
2022-04-18 23:32:50 +0100

    no gap on mobile

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,7 +11,7 @@ const ImageContainer:FC<{work: Work}> = ({work}) => {
     const gridTemplateColumns = useIsMobile() ? '1fr 16fr 1fr' : '1fr 1fr 1fr'
     return  <div style={{display: 'grid', gridTemplateColumns: gridTemplateColumns, width: '100%'}}>
     <div style={{gridColumnStart: 2, gridColumnEnd: 3}} onClick={onClick}>
-        <img src={work.image} width={'100%'} alt={work.alt} style={{marginBottom: 0}}/>
+        <img src={work.image} width={'100%'} alt={work.alt} style={{marginBottom: -5, paddingTop: 20}}/>
     </div>
     {!useIsMobile() && <div style={{gridColumnStart: 3, gridColumnEnd: 4, display: 'flex', flexDirection: 'column', justifyContent: 'flex-end',  marginLeft: '15px', fontSize: 'small', fontStyle:'italic'}}>
         <p style={{margin: '0', fontWeight: 'bold'}}>
@@ -25,10 +25,11 @@ const ImageContainer:FC<{work: Work}> = ({work}) => {
 }
 
 export const Home:FC<{works: Array<Work>}> = ({works}) => {
+    const marginTop = useIsMobile() ? '20px' : '100px'
     return <div style={{border: '0.5px solid white'}}>
         <EmptyBar/>
         <NavBar/>
-        <div style={{color: "black", display: 'flex', flexDirection: 'column', justifyContent: 'center', gap:'20px', alignItems: 'center', fontSize: 'larger', marginBottom: '10px', marginTop: '100px'}}>
+        <div style={{color: "black", display: 'flex', flexDirection: 'column', justifyContent: 'center', gap:'20px', alignItems: 'center', fontSize: 'larger', marginBottom: '10px', marginTop}}>
             {works.map((work) => <ImageContainer work={work}/>)}
         </div>
     </div>
