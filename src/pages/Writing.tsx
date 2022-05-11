ef0894d7fbbf5c0ef64002d2f5128adeace40176
Jonathan Raphael <jonathan.raphael274@gmail.com>
2022-05-11 18:04:18 +0300

    adds p tag to writing page

--- src/pages/Writing.tsx
+++ src/pages/Writing.tsx
@@ -4,8 +4,13 @@ import { NavBar } from '../components/NavBar'
 export const Writing:FC = () => {
     return <div style={{width: '100vw', height: '100vh'}}>
          <NavBar/>
-         <div style={{width: '100%', textAlign: 'center', height: '40%', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-            Writing
+         <div style={{width: '100%', textAlign: 'center', height: '40%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+             <h1>
+                Writing 
+             </h1>
+             <p>
+                 hehehh
+             </p>
          </div>
     </div>
 }
