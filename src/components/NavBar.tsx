616f558e24613a4a6575c5d2c34f745d0e050495
Kiavash <kiavash89@gmail.com>
2022-05-16 10:50:18 +0100

    added mobile and desktop style objects for navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,30 @@ import useIsMobile from '../services/useIsMobile'
 
 const pages = [{route: '/home', title: 'Nick Collee'},{route:'/works', title: 'Works'},{ route: '/writing', title: 'Writing'},{ route: '/shop', title: 'Shop'}, {route: '/contact', title: 'Contact'}]
 
+const mobileStyles = {
+    minWidth: '100%',
+    justifyContent: 'space-evenly',
+    marginTop: '20px',
+    marginBottom: '25px',
+    marginLeft: 'initial',
+    flexDirection: 'row',
+    position: 'initial'
+}
+
+const desktopStyles = {
+    minWidth: 'initial',
+    justifyContent: 'initial',
+    marginTop: '100px',
+    marginBottom: 'initial',
+    marginLeft: '60px',
+    flexDirection: 'column',
+    position: 'fixed'
+}
+
 export const NavBar: FC = () => {
+
+    const styles = useIsMobile() ? mobileStyles : desktopStyles
+
     if (useIsMobile()) {
         return <div style={{minWidth: '100%', justifyContent: 'space-evenly', marginTop: '20px', display: 'flex', flexDirection: 'row', gap: 20, marginBottom: '25px'}}>
         {pages.map(page => <Link to={page.route} style={{textDecoration: 'none', color: 'black'}}>
--- src/services/works.ts
+++ src/services/works.ts
@@ -98,20 +98,9 @@ export const works = [concreteShoe, lino, fields, spoon, portait]
 
 export const writingPageValues = {
     writingHeading: 'A theatrical exhibition by the Turner Prize winning artist and cultural activist',
-    paragraphs: ['This large-scale exhibition will debut recent work and include selected highlights from Lubaina Himid’s influential career. Taking inspiration from her interest in \
-    theatre, the exhibition will unfold in a sequence of scenes designed to place visitors centre-stage \
-    and backstage.', 'Initially trained in theatre design, Himid is known for her \
-    innovative approaches to painting and to social engagement. She has been \
-    pivotal in the UK since the 1980s for her contributions to the British \
-    Black arts movement, making space for the expression and recognition \
-    of Black experience and womens creativity. Over the last decade, she has \
-    of Black experience and women\'s creativity. Over the last decade, she has \
-    earned international recognition for her figurative paintings, which explore \
-    overlooked and invisible aspects of history and of contemporary everyday life. \
-    In 2017 she was awarded the Turner Prize and in 2018 she was bestowed with the \
-     honorary title of CBE for her contributions to the arts.'],
-    italicText: 'Exhibition organised by Tate Modern in collaboration with Musée \
-    cantonal des Beaux-Arts de Lausanne/Plateforme 10',
+    paragraphs: ['This large-scale exhibition will debut recent work and include selected highlights from Lubaina Himid’s influential career. Taking inspiration from her interest in theatre, the exhibition will unfold in a sequence of scenes designed to place visitors centre-stage and backstage.',
+    'Initially trained in theatre design, Himid is known for her innovative approaches to painting and to social engagement. She has been pivotal in the UK since the 1980s for her contributions to the British Black arts movement, making space for the expression and recognition of Black experience and womens creativity. Over the last decade, she has of Black experience and women\'s creativity. Over the last decade, she has earned international recognition for her figurative paintings, which explore overlooked and invisible aspects of history and of contemporary everyday life. In 2017 she was awarded the Turner Prize and in 2018 she was bestowed with the honorary title of CBE for her contributions to the arts.'],
+    italicText: 'Exhibition organised by Tate Modern in collaboration with Musée cantonal des Beaux-Arts de Lausanne/Plateforme 10',
     image: writingPageImage,
     imageCaption: 'Lubaina Himid <i>Six Tailors</i> 2019 Rennie Collection, Vancouver &copy; Lubaina Himid',
     imageAltText: 'placeholder'
