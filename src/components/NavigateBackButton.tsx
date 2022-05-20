44340e52c0678552ee718e9cd23375316811dc66
Kiavash <kiavash89@gmail.com>
2022-05-20 16:09:05 +0100

    created mobile page style for navigation back button and inidividual work pieces

--- src/components/NavigateBackButton.tsx
+++ src/components/NavigateBackButton.tsx
@@ -3,13 +3,16 @@ import { useNavigate, useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeftLong } from '@fortawesome/free-solid-svg-icons';
 import './NavigateBackButton.css'
+import useIsMobile from '../services/useIsMobile'
+
 
 export const NavigateBackButton: FC = () => {
     const navigate = useNavigate();
+    const id = useIsMobile() ? 'mobile-button' : 'button'
     let location = useLocation();
 
     return <div>
-    <FontAwesomeIcon id='button' icon={faArrowLeftLong} size='2x' onClick={() => {
+    <FontAwesomeIcon id={id} icon={faArrowLeftLong} size='2x' onClick={() => {
         if (location.state === 'works') navigate('/works')
         else navigate('/home')
         window.scrollTo(0, 0);
--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -2,26 +2,52 @@ import {FC, useState} from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 import { RelatedImage, Work } from '../services/works';
 import { NavigateBackButton } from '../components/NavigateBackButton'
+import useIsMobile from '../services/useIsMobile'
+
+
+const mobileStyles = {
+    width: '92%',
+    bodyPadding: '0 10vw 0 10vw',
+    paragraphMaxWidth: 'initial',
+    gap: 20,
+    paddingTop: 10,
+    fontSize: '0.85rem',
+    subheadingTopMargin: '-15px'
+
+}
+
+const desktopStyles = {
+    width: '60%',
+    bodyPadding: 'initial',
+    paragraphMaxWidth: '60%',
+    gap: 60,
+    paddingTop: 40,
+    fontSize: 'initial',
+    subheadingTopMargin: 'initial'
+}
+
 
 const ProjectContent: FC<{work: Work}> = ({work}) => {
+
+    const styles = useIsMobile() ? mobileStyles : desktopStyles
     const workAsRelated: RelatedImage = {image: work.image, caption: work.imageCaption}
     const [currentImage, setCurrentImage] = useState<RelatedImage>(workAsRelated)
     const carousel = [{image: work.image, caption: work.caption}, ...work.relatedImages]
     return <div>
-        <NavigateBackButton/>
-        <div style={{width: '60%', display: "inline-flex", flexDirection: "column", alignItems: 'center', gap: 60, paddingTop: 40 }}>
+        <NavigateBackButton />
+        <div style={{width: styles.width, display: "inline-flex", flexDirection: "column", alignItems: 'center', gap: styles.gap, paddingTop: styles.paddingTop, fontSize: styles.fontSize}}>
             <div>
                 <h1>{work.id.toUpperCase()}</h1>
-                <p>{work.caption}</p>
+                <p style={{marginTop: styles.subheadingTopMargin}}>{work.caption}</p>
             </div>
-            <div style={{maxWidth: '60%'}} >
+            <div style={{maxWidth: styles.width, padding: styles.bodyPadding}} >
                 <img src={currentImage.image} alt={currentImage.caption} width={'100%'} style={{}}/>
                 <p style={{textAlign:'left', fontSize: 'small', margin: 0}}>{currentImage.caption}</p>
                 <div style={{display: 'flex', marginTop: '20px'}}>
                     {carousel.map(related => <img src={related.image} alt={related.caption} style={{width: '20%', objectFit: 'cover'}} onClick={() => setCurrentImage(related)}/>)}
                 </div>
             </div>
-            <p style={{maxWidth: '60%', fontSize: 'small'}}>{work.description}</p>
+            <p style={{padding: styles.bodyPadding, maxWidth: styles.paragraphMaxWidth, fontSize: 'small'}}>{work.description}</p>
         </div>
     </div>
 }
