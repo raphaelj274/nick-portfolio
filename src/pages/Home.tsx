aa6beae0ebf8ca46649127da7cfeef543e797be0
Kiavash <kiavash89@gmail.com>
2022-05-20 12:39:26 +0100

    added fontawesome package, back navigation button for projects, and set scrollTo to 0 when navigating in/out of projects

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,31 @@
-import {FC} from 'react' 
+import {FC} from 'react'
 import { Work } from '../services/works'
 import { EmptyBar } from '../components/EmptyBar'
 import { NavBar } from '../components/NavBar'
 import useIsMobile from '../services/useIsMobile'
 import { useNavigate } from 'react-router-dom';
 
+
 const ImageContainer:FC<{work: Work}> = ({work}) => {
     const navigate = useNavigate();
-    const onClick = () => navigate(`/project/${work.id}`)
+    const onClick = () => {
+        navigate(`/project/${work.id}`)
+        window.scrollTo(0, 0)
+    }
     const gridTemplateColumns = useIsMobile() ? '1fr 16fr 1fr' : '1fr 1fr 1fr'
     return  <div style={{display: 'grid', gridTemplateColumns: gridTemplateColumns, width: '100%' }}>
-    <div style={{gridColumnStart: 2, gridColumnEnd: 3}} onClick={onClick}>
-        <img src={work.image} width={'100%'} alt={work.alt} style={{marginBottom: -5}}/>
-    </div>
-    {!useIsMobile() && <div style={{gridColumnStart: 3, gridColumnEnd: 4, display: 'flex', flexDirection: 'column', justifyContent: 'flex-end',  marginLeft: '15px', fontSize: 'small', fontStyle:'italic'}}>
-        <p style={{margin: '0', fontWeight: 'bold'}}>
-                    {work.date}
-        </p>
-        <p style={{margin: '0'}}>
-                    {work.caption}
-        </p>
-    </div>}
-</div>
+                <div style={{gridColumnStart: 2, gridColumnEnd: 3}} onClick={onClick}>
+                    <img src={work.image} width={'100%'} alt={work.alt} style={{marginBottom: -5}}/>
+                </div>
+                {!useIsMobile() && <div style={{gridColumnStart: 3, gridColumnEnd: 4, display: 'flex', flexDirection: 'column', justifyContent: 'flex-end',  marginLeft: '15px', fontSize: 'small', fontStyle:'italic'}}>
+                    <p style={{margin: '0', fontWeight: 'bold'}}>
+                        {work.date}
+                    </p>
+                    <p style={{margin: '0'}}>
+                        {work.caption}
+                    </p>
+                </div>}
+            </div>
 }
 
 export const Home:FC<{works: Array<Work>}> = ({works}) => {
@@ -33,5 +37,5 @@ export const Home:FC<{works: Array<Work>}> = ({works}) => {
             {works.map((work) => <ImageContainer work={work}/>)}
         </div>
     </div>
-    
+
 }
--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,24 +1,34 @@
-import {FC, useState} from 'react' 
+import {FC, useState} from 'react'
 import { Navigate, useParams } from 'react-router-dom'
 import { RelatedImage, Work } from '../services/works'
+import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeftLong } from '@fortawesome/free-solid-svg-icons';
 
 const ProjectContent: FC<{work: Work}> = ({work}) => {
     const workAsRelated: RelatedImage = {image: work.image, caption: work.imageCaption}
     const [currentImage, setCurrentImage] = useState<RelatedImage>(workAsRelated)
     const carousel = [{image: work.image, caption: work.caption}, ...work.relatedImages]
-    return <div style={{width: '60%', display: "flex", flexDirection: "column", alignItems: 'center', gap: 60, paddingTop: 40 }}>
-        <div>
-            <h1>{work.id.toUpperCase()}</h1>
-            <p>{work.caption}</p>
-        </div>
-        <div style={{maxWidth: '60%'}} >
-            <img src={currentImage.image} alt={currentImage.caption} width={'100%'} style={{}}/>
-            <p style={{textAlign:'left', fontSize: 'small', margin: 0}}>{currentImage.caption}</p>
-            <div style={{display: 'flex', marginTop: '20px'}}>
-                {carousel.map(related => <img src={related.image} alt={related.caption} style={{width: '20%', objectFit: 'cover'}} onClick={() => setCurrentImage(related)}/>)}
+    const navigate = useNavigate();
+    return <div>
+        <FontAwesomeIcon icon={faArrowLeftLong} size='2x' style={{position: 'fixed', top: 68, left: 200, cursor: 'pointer'}} onClick={() => {
+        navigate('/home')
+        window.scrollTo(0, 0);
+        }}/>
+        <div style={{width: '60%', display: "inline-flex", flexDirection: "column", alignItems: 'center', gap: 60, paddingTop: 40 }}>
+            <div>
+                <h1>{work.id.toUpperCase()}</h1>
+                <p>{work.caption}</p>
             </div>
+            <div style={{maxWidth: '60%'}} >
+                <img src={currentImage.image} alt={currentImage.caption} width={'100%'} style={{}}/>
+                <p style={{textAlign:'left', fontSize: 'small', margin: 0}}>{currentImage.caption}</p>
+                <div style={{display: 'flex', marginTop: '20px'}}>
+                    {carousel.map(related => <img src={related.image} alt={related.caption} style={{width: '20%', objectFit: 'cover'}} onClick={() => setCurrentImage(related)}/>)}
+                </div>
+            </div>
+            <p style={{maxWidth: '60%', fontSize: 'small'}}>{work.description}</p>
         </div>
-        <p style={{maxWidth: '60%', fontSize: 'small'}}>{work.description}</p>
     </div>
 }
 
