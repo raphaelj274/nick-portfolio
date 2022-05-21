f528099ac6d4545c0663e8feec6754749ef98973
Kiavash <kiavash89@gmail.com>
2022-05-21 18:05:02 +0100

    moved empty bar up to remove gap at the top of the window

--- src/components/EmptyBar.tsx
+++ src/components/EmptyBar.tsx
@@ -1,6 +1,6 @@
-import {FC} from 'react' 
+import {FC} from 'react'
 
 export const EmptyBar:FC = () => {
-    return <div style={{height: '20px', width: '100%', position: 'fixed', backgroundColor: 'white'}}>
+    return <div style={{height: '20px', width: '100%', position: 'fixed', backgroundColor: 'white', top: '0px'}}>
     </div>
 }
