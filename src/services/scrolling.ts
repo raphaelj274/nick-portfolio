009910fba76a9da1a1e14776cc91eb08dc3a0eda
Kiavash <kiavash89@gmail.com>
2022-05-22 19:01:50 +0100

    added comment to scrolltotop function for clarity

--- src/services/scrolling.ts
+++ src/services/scrolling.ts
@@ -7,7 +7,8 @@ const scrollToHash = (project: string) : void => {
 const scrollToTop = () : void => {
 
   // we can modify this function in future if the scroll to top behaviour doesn't work properly...
-  // ...on certain devices - not an issue for now
+  // ... on certain devices - not an issue for now as scroller position being carried from page to page...
+  // ... doesn't happen on mobiles due to the navbar being at the top
 
   window.scrollTo(0, 0)
 }
