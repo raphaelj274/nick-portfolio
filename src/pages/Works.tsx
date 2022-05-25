e0aaf271e01395e50c83935fdd3f19e3f60d5ee2
Kiavash <kiavash89@gmail.com>
2022-05-25 15:26:31 +0100

    made greater use of space on smaller desktop sizes

--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -5,17 +5,20 @@ import { Work } from '../services/works'
 import StackGrid from "react-stack-grid";
 import { scrollToTop } from '../services/scrolling';
 import useIsMobile from '../services/useIsMobile';
+import { EmptyBar } from '../components/EmptyBar';
 
 const mobileStyles = {
     gridMargin: '30px auto auto auto',
     width: '95%',
-    columnWidth: '40%'
+    minWidth: 'initial',
+    columnWidth: '36%'
 
 }
 
 const desktopStyles = {
     gridMargin: '100px auto auto 200px',
     width: 'calc(100% - 380px)',
+    minWidth: '68%',
     columnWidth: '33%'
 }
 
@@ -40,10 +43,11 @@ export const Works:FC<{works: Array<Work>}> = ({works}) => {
     const styles = useIsMobile() ? mobileStyles : desktopStyles
 
     return <div style={{maxWidth: '100%', width: '100vw', height: '100vh'}}>
+         <EmptyBar/>
          <NavBar/>
          <div style={{border: '1px solid white'}}>
          </div>
-            <StackGrid columnWidth={styles.columnWidth} style={{boxSizing: 'border-box', width: styles.width, margin: styles.gridMargin, overflow: 'hidden'}}>
+            <StackGrid columnWidth={styles.columnWidth} style={{boxSizing: 'border-box', width: styles.width, margin: styles.gridMargin, overflow: 'hidden', minWidth: styles.minWidth}}>
                 {works.map(work => <ContactElement work={work}/>)}
                 {works.map(work => <ContactElement work={work}/>)}
                 {works.map(work => <ContactElement work={work}/>)}
