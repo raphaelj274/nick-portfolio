c5ab5978bc9b345bef64524029358914860f3d43
Kiavash <kiavash89@gmail.com>
2022-05-25 15:04:46 +0100

    first working version of works mobile page

--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -4,6 +4,21 @@ import { NavBar } from '../components/NavBar'
 import { Work } from '../services/works'
 import StackGrid from "react-stack-grid";
 import { scrollToTop } from '../services/scrolling';
+import useIsMobile from '../services/useIsMobile';
+
+const mobileStyles = {
+    gridMargin: '30px auto auto auto',
+    width: '95%',
+    columnWidth: '40%'
+
+}
+
+const desktopStyles = {
+    gridMargin: '100px auto auto 200px',
+    width: 'calc(100% - 380px)',
+    columnWidth: '33%'
+}
+
 
 
 const ContactElement:FC<{work: Work}> = ({work}) => {
@@ -21,11 +36,14 @@ const ContactElement:FC<{work: Work}> = ({work}) => {
 
 
 export const Works:FC<{works: Array<Work>}> = ({works}) => {
-    return <div style={{width: '100vw', height: '100vh'}}>
+
+    const styles = useIsMobile() ? mobileStyles : desktopStyles
+
+    return <div style={{maxWidth: '100%', width: '100vw', height: '100vh'}}>
          <NavBar/>
          <div style={{border: '1px solid white'}}>
          </div>
-            <StackGrid columnWidth={300} style={{width: '1000px', marginTop: '100px' , marginLeft: '300px', overflow: 'hidden'}}>
+            <StackGrid columnWidth={styles.columnWidth} style={{boxSizing: 'border-box', width: styles.width, margin: styles.gridMargin, overflow: 'hidden'}}>
                 {works.map(work => <ContactElement work={work}/>)}
                 {works.map(work => <ContactElement work={work}/>)}
                 {works.map(work => <ContactElement work={work}/>)}
