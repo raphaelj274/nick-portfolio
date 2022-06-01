62c503130f29410811bdbcda76ba7c3e49ed3317
Kiavash <kiavash89@gmail.com>
2022-06-01 17:05:31 +0100

    made more presentable

--- src/pages/Writing.tsx
+++ src/pages/Writing.tsx
@@ -8,16 +8,14 @@ import { scrollToTop } from '../services/scrolling'
 const mobileStyles = {
     gridTemplateColumns: '1fr',
     textAlignment: 'center' as const,
-    gridGap: '40px',
     gridMargins: '0 auto auto auto',
     gridMaxWidth: '90%',
     headingMargin : '0 0 23px 0',
 }
 
 const desktopStyles = {
-    gridTemplateColumns: '0.3fr 3fr 1fr',
+    gridTemplateColumns: '0.9fr 3fr 1.6fr',
     textAlignment: 'left' as const,
-    gridGap: '80px',
     gridMargins: 'auto 60px auto 230px',
     gridMaxWidth: 'calc(100% - 260px)',
     headingMargin : '92px auto 24px auto',
@@ -31,15 +29,15 @@ export const Writing:FC<{writingContent: WritingContent}> = ({writingContent}) =
 
     return <div style={{maxWidth: '100%', width: '100vw', paddingBottom: '30px'}}>
         <NavBar/>
-        <div style={{maxWidth: styles.gridMaxWidth, width: '100%', textAlign: styles.textAlignment, margin: styles.gridMargins, display: 'grid', gridGap: styles.gridGap, gridTemplateColumns: styles.gridTemplateColumns}}>
+        <div style={{maxWidth: styles.gridMaxWidth, width: '100%', textAlign: styles.textAlignment, margin: styles.gridMargins, display: 'grid', gridTemplateColumns: styles.gridTemplateColumns}}>
             <div style={{gridColumn: '2', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'start'}}>
                 <h1 style={{margin: styles.headingMargin}}>
                 {writingContent.heading}
                 </h1>
                 {writingContent.paragraphs.map(x => {
-                    return <p style={{marginTop: '0px'}}>{x}</p>
+                    return <p style={{marginTop: '5px'}}>{x}</p>
                 })}
-                <p style={{marginTop: '0px'}}>
+                <p style={{marginTop: '5px'}}>
                 <i>{writingContent.italicText}</i>
                 </p>
                 <div style={{width: '78%'}} id={'img'}>
